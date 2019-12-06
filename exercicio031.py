618ecfc81ab84b2090e3abe6d7624b051fc53796
carlosfariasebix <58307467+carlosfariasebix@users.noreply.github.com>
2019-12-06 17:29:04 -0300

    commited

--- exercicio031.py
+++ exercicio031.py
@@ -1,9 +1,11 @@
-km = int(input("Digite a distancia da sua viagem em KM : "))
+km = float(input("Digite a distancia da sua viagem em KM : "))
 media= 200
 preço1= 0.50
 preço2= 0.45
 
+print("O valor da sua viagem é de {} reias.".format(km * preço1) if km <= media else "O valor da sua viagem é de {:.2f} reias.".format(km * preço2))
+
 if km <= media:
     print("O valor da sua viagem é de {} reias.".format(km * preço1))
 else:
-    print("O valor da sua viagem é de {} reias.".format(km * preço2))
+    print("O valor da sua viagem é de {:.2f} reias.".format(km * preço2))

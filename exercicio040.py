007e427e070ba5c1825dd5c1a1f1ef69217a614d
Carlos Lima <Carllosaugusto@outlook.com>
2019-12-12 00:00:26 -0300

    commitando pelo Paycharm

---
+++ exercicio040.py
@@ -0,0 +1,13 @@
+n1 = float(input("Nota 1: "))
+n2 = float(input("Nota 2: "))
+
+media = (n1 + n2) / 2
+
+if media < 5.0:
+    print("REPROVADO")
+elif media >= 5.0 and media <= 6.9:
+    print("RECUPERAÇÃO")
+else:
+    print("APROVADO")
+
+print(media)
